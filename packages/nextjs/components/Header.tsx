33bb3650e6cc9094a065fa84fb623b2c7f7aa929
MichalisG <michalis3489@gmail.com>
2024-11-17 04:01:09 +0700

    fix: header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
   const { address } = useAccount();
 
   return (
-    <div className="sticky top-0 navbar bg-base-100 min-h-20 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 items-center">
+    <div className="sticky top-0 navbar bg-white min-h-20 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 items-center">
       <div className="flex justify-between w-full">
         <div className="flex justify-start">{address && <Balance connectedAddress={address} />}</div>
         <div className="flex justify-end items-center p-2 mt-2">
