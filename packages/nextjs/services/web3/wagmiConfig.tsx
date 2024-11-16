e518b0a03c02e081aa5c0be736d50ef86c382450
MichalisG <michalis3489@gmail.com>
2024-11-16 22:28:29 +0700

    fix: wagmi config

--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -1,6 +1,7 @@
 import { wagmiConnectors } from "./wagmiConnectors";
+import { injectedWithSapphire, sapphireHttpTransport } from "@oasisprotocol/sapphire-wagmi-v2";
 import { Chain, createClient, fallback, http } from "viem";
-import { hardhat, mainnet } from "viem/chains";
+import { hardhat, mainnet, sapphireTestnet } from "viem/chains";
 import { createConfig } from "wagmi";
 import scaffoldConfig from "~~/scaffold.config";
 import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
@@ -13,21 +14,11 @@ export const enabledChains = targetNetworks.find((network: Chain) => network.id 
   : ([...targetNetworks, mainnet] as const);
 
 export const wagmiConfig = createConfig({
-  chains: enabledChains,
-  connectors: wagmiConnectors,
+  multiInjectedProviderDiscovery: false,
   ssr: true,
-  client({ chain }) {
-    const alchemyHttpUrl = getAlchemyHttpUrl(chain.id);
-    const rpcFallbacks = alchemyHttpUrl ? [http(alchemyHttpUrl), http()] : [http()];
-
-    return createClient({
-      chain,
-      transport: fallback(rpcFallbacks),
-      ...(chain.id !== (hardhat as Chain).id
-        ? {
-            pollingInterval: scaffoldConfig.pollingInterval,
-          }
-        : {}),
-    });
+  chains: [sapphireTestnet],
+  connectors: [injectedWithSapphire()],
+  transports: {
+    [sapphireTestnet.id]: sapphireHttpTransport(),
   },
 });
